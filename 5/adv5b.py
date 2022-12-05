461326f53f6c0a82aabde4f26becf560ffde670e
BjoWerg <bjorn.wergeland@telia.com>
2022-12-05 14:14:48 +0100

    Compact code

--- 5/adv5b.py
+++ 5/adv5b.py
@@ -7,11 +7,9 @@ with open('input5.txt') as crates:
 			if line[1] == '1':
 				setup = False
 			else:
-				pos = 0
-				for ch in line:
-					if (pos-1)%4 == 0 and ch != ' ':
-						stack[(pos-1)//4] = [ch] + stack[(pos-1)//4]
-					pos += 1
+				for pos in range(len(line)):
+					if (pos-1)%4 == 0 and line[pos] != ' ':
+						stack[(pos-1)//4] = [line[pos]] + stack[(pos-1)//4]
 		elif 'move' in line:
 			moves = line.split(' ')
 			crane = []
